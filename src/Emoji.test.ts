fb706b8fde29ce01c877f272ed6bea01993ec6f6
Sean McPherson <seanmcp25@gmail.com>
2019-01-05 11:15:32 -0500

    Add tests

---
+++ src/Emoji.test.ts
@@ -0,0 +1,23 @@
+import Emoji from './Emoji';
+
+const props = {
+    label: 'A test tube with bubbly green contents',
+    symbol: '🧪'
+};
+
+test('Is a defined function', () => {
+    expect(Emoji).not.toBe(undefined);
+    expect(typeof Emoji).toBe('function');
+});
+
+test('Sets aria-hidden, -label when label prop is NOT passed', () => {
+    const output = Emoji({ symbol: props.symbol });
+    expect(output.props['aria-hidden']).toMatch('true');
+    expect(output.props['aria-label']).toBe(null);
+});
+
+test('Sets aria-hidden, -label when label prop IS passed', () => {
+    const output = Emoji(props);
+    expect(output.props['aria-hidden']).toMatch('false');
+    expect(output.props['aria-label']).toMatch(props.label);
+});
--- src/Emoji.tsx
+++ src/Emoji.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 interface EmojiProps {
-    label: string;
+    label?: string;
     symbol: string;
 }
 
