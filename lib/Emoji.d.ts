b8dac90b9a6d6a2c28b24892e3565ad05af55118
Sean McPherson <seanmcp25@gmail.com>
2019-01-05 10:54:53 -0500

    Build and export

---
+++ lib/Emoji.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+interface EmojiProps {
+    label: string;
+    symbol: string;
+}
+declare function Emoji(props: EmojiProps): JSX.Element;
+export default Emoji;
---
+++ main.js
@@ -0,0 +1,1 @@
+exports.Emoji = require("./lib/Emoji");
