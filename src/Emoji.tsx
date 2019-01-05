ffedbdcbbd1df04fa18203dcd86afdcf848a25dc
Sean McPherson <seanmcp25@gmail.com>
2019-01-05 10:54:45 -0500

    Update Emoji

--- src/Emoji.tsx
+++ src/Emoji.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import * as React from 'react';
 
-const Emoji = ({ label, symbol, ...props }) => {
+interface EmojiProps {
+    label: string;
+    symbol: string;
+}
+
+function Emoji(props: EmojiProps) {
+    const { label, symbol, ...rest } = props;
     return (
         <span
             aria-hidden={label ? 'false' : 'true'}
             aria-label={label ? label : null}
             role="img"
-            {...props}
+            {...rest}
         >
             {symbol}
         </span>
     );
-};
-
-Emoji.propTypes = {
-    label: PropTypes.string,
-    symbol: PropTypes.string.isRequired
 };
 
 export default Emoji;
